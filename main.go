2542d6e83e31814df1fdb9ecec613c6248833975
Seregk4 <abrsera@gmail.com>
2024-10-04 21:25:33 +0300

    Первый коммит

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var message string
+
+type requestBody struct {
+	Message string `json:message`
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	var reqBody requestBody
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	if err != nil {
+		return
+	}
+	message = reqBody.Message
+}
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello,"+message+"!")
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/hello", GetHandler).Methods("GET")
+	http.ListenAndServe(":8080", router)
+}
